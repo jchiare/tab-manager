bbcc78e54114e81253e60aebcaf56903aee00f34
Jay Chiarella <jay.gch93@gmail.com>
2022-12-25 19:33:35 -0500

    update bakcgroundjs

--- src/public/background.js
+++ src/public/background.js
@@ -1,1 +1,8 @@
-console.log('heyyy');
+async function getCurrentTab(activeInfo) {
+    const currentTab = await chrome.tabs.get(activeInfo.tabId);
+    console.log(currentTab);
+    return currentTab;
+}
+
+chrome.tabs.onActivated.addListener(activeInfo => getCurrentTab(activeInfo).then(result => console.log(result)));
+console.log('backgrrounds.js');
