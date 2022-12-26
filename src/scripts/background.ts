1b553155d7151d877125ed760583836e7bc88374
Jay Chiarella <jay.gch93@gmail.com>
2022-12-25 22:18:29 -0500

    fix setting the storage cue

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,23 +1,16 @@
-function getStorageData(key: string) {
-    new Promise((resolve, reject) => chrome.storage.sync.get(key, result => (chrome.runtime.lastError ? reject(Error(chrome.runtime.lastError.message)) : resolve(result))));
-}
-function setStorageData(data: any) {
-    new Promise((resolve, reject) => chrome.storage.sync.set(data, () => (chrome.runtime.lastError ? reject(Error(chrome.runtime.lastError.message)) : resolve())));
-}
-
 async function getCurrentTab(activeInfo: chrome.tabs.TabActiveInfo) {
     const currentTab = await chrome.tabs.get(activeInfo.tabId);
     const { url } = currentTab;
 
-    const data = await chrome.storage.sync.get([url]);
-    console.log('data: ', data);
-    console.log('stringify data: ', JSON.stringify(data));
+    if (!url) {
+        throw new Error('Missing url');
+    }
+    const data = await chrome.storage.sync.get(url);
     if (data.url) {
         console.log(`found data "${JSON.stringify(data)}" from url "${url}"`);
     } else {
-        console.log(`no data for "${url}"`);
-        const setResult = await chrome.storage.sync.set({ url: 'hye' });
-        console.log(JSON.stringify(setResult));
+        console.log(await chrome.storage.sync.get(null));
+        await chrome.storage.sync.set({ [url]: 'hye' });
     }
     return currentTab;
 }
