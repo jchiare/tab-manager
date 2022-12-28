49cffd2f1c3f0e4d6b70526fbd9fc4a56ad7098e
Jay Chiarella <jay.gch93@gmail.com>
2022-12-27 21:45:17 -0500

    fix small html/css issues

--- src/scripts/removeOldTabs.ts
+++ src/scripts/removeOldTabs.ts
@@ -5,7 +5,7 @@ async function getActiveTab(): Promise<chrome.tabs.Tab> {
 }
 
 async function sendMessageToServiceWorker(args: any[]) {
-    const [activeTabId, expiryTime] = args;
+    const [activeTabId, expiryTime, timeRange] = args;
     const message = { activeTabId, expiryTime };
     await chrome.runtime.sendMessage(message);
 }
@@ -13,21 +13,24 @@ async function sendMessageToServiceWorker(args: any[]) {
 async function removeOldTabs() {
     const activeTab = await getActiveTab();
     if (!activeTab.id) return;
+
     try {
         await chrome.scripting.executeScript<any, any>({
             target: { tabId: activeTab.id },
             func: sendMessageToServiceWorker,
-            args: [[activeTab.id, expiryTimeInput.value]]
+            args: [[activeTab.id, expiryTimeValueInput.value, expiryTimeRangeInput.value]]
         });
     } catch (error) {
         if (typeof error === 'string' && error.includes('Error: Cannot access a chrome:// URL')) {
             console.log('skipping executing script on a new tab');
             return;
         }
+        throw new Error(error);
     }
 }
 
 const removeOldTabsBtn = document.getElementById('removeOldTabs-btn')!;
 removeOldTabsBtn.addEventListener('click', removeOldTabs);
 
-const expiryTimeInput = <HTMLInputElement>document.getElementById('old-tab-expiration')!;
+const expiryTimeValueInput = <HTMLInputElement>document.getElementById('stale-tab-expiration')!;
+const expiryTimeRangeInput = <HTMLSelectElement>document.getElementById('stale-tab-time')!;
